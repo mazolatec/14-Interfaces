0b630abb864c9befcd60c9a29dd31d5ace94010b
mazolatec <mazolatec@gmail.com>
2022-11-13 08:02:08 -0300

    Add app AluguelCarrosSemInterface

---
+++ INTERFACES/01-AluguelCarrosSemInterface/Entities/Invoice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_AluguelCarrosSemInterface.Entities
+{
+    internal class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double basicPayment, double tax)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+        public double totalPayment{
+            get { return BasicPayment + Tax; }
+
+        }
+
+        public override string ToString()
+        {
+            return "Basic payment: "
+                + BasicPayment.ToString("F2", CultureInfo.CurrentCulture)
+                + "\nTax: "
+                + Tax.ToString("F2", CultureInfo.CurrentCulture)
+                + "\nTotal Payment: "
+                + totalPayment.ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+
+    }
+}
---
+++ INTERFACES/01-AluguelCarrosSemInterface/Services/RentalService.cs
@@ -0,0 +1,36 @@
+﻿
+
+using _01_AluguelCarrosSemInterface.Entities;
+
+namespace _01_AluguelCarrosSemInterface.Services
+{
+    internal class RentalService
+    {
+        public double PricePerHour  { get; set; }
+        public double PricePerDay  { get; set; }
+        public BrasilTaxService _brasilTaxService = new BrasilTaxService();
+
+        public RentalService(double pricePerHour, double pricePerDay)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+        }
+
+        public void processInvoice(CarRental carRental)
+        {
+            TimeSpan duraction=carRental.Finish.Subtract(carRental.Start);
+            double basicPayment = 0.0;
+            if (duraction.Hours <=12)
+            {
+                basicPayment = PricePerHour * Math.Ceiling( duraction.TotalHours);
+            }
+            else
+            {
+                basicPayment=PricePerDay * Math.Ceiling( duraction.TotalDays);
+            }
+            double tax = _brasilTaxService.Tax(basicPayment);
+            carRental.Invoice =new Invoice(basicPayment, tax);
+
+        }
+    }
+}
